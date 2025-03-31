8ac798b32c4a3d13c8df7ca5d5bcc7570f5f6a2e
GANESH312006 <ganeshmurthy2020@outlook.com>
2025-03-31 20:37:14 +0530

    added new popup effect

---
+++ GDSC_events_page/hover.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const images = document.querySelectorAll('.card');
+
+images.forEach(function (img) {
+  img.addEventListener('mouseover', function (e) {
+    if (e.target.matches(':hover')) {
+      this.querySelector('.slide-content').classList.remove('slide-hidden');
+    }
+  });
+});
+
+images.forEach(function (img) {
+  img.addEventListener('mouseleave', function (e) {
+    this.querySelector('.slide-content').classList.add('slide-hidden');
+  });
+});
